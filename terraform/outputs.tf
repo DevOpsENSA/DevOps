eeda0279c1695a80aaaef5353df4bdf5cec83eaa
Ilyass Cherradi <ilyasscherradi15@gmail.com>
2026-04-27 18:32:20 +0100

    Add terraform files

---
+++ terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "frontend_url" {
+  value       = "http://localhost:4200"
+  description = "URL du Frontend Angular"
+}
+
+output "backend_url" {
+  value       = "http://localhost:8080"
+  description = "URL du Backend Laravel"
+}
+
+output "database_port" {
+  value       = "5433"
+  description = "Port PostgreSQL"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "db_password" {
+  description = "Mot de passe PostgreSQL"
+  default     = "admin123"
+}
+
+variable "db_name" {
+  description = "Nom de la base de données"
+  default     = "plateformEtudiant"
+}
+
+variable "db_user" {
+  description = "Utilisateur PostgreSQL"
+  default     = "admin"
+}
